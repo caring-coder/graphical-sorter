ed33cb704fcb4a651a216f1d0e7b46423beb934d
Joseph Verron <joseph.verron@gmail.com>
2022-06-12 00:58:11 +0200

    update

--- player.py
+++ player.py
@@ -1,8 +1,8 @@
-import os
-from os import path
+from os import path, listdir, rename, makedirs, system
 
 from PyQt5.QtCore import QUrl, Qt, QRect
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLayout, QStackedLayout, QHBoxLayout, QLabel, QPushButton, \
+    QGridLayout, QWidget, QVBoxLayout
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
 
@@ -18,31 +18,56 @@ def walk(dir_path):
 def move_file_to_dir(source_path, target_dir):
     source_basename = path.basename(source_path)
     target_path = path.join(target_dir, source_basename)
-    os.makedirs(target_dir, exist_ok=True)
+    makedirs(target_dir, exist_ok=True)
     try:
-        os.rename(source_path, target_path)
+        rename(source_path, target_path)
     except FileNotFoundError as err:
         print(err)
+    return path.getsize(target_dir)
 
+
+def get_size_format(b, factor=1024, suffix="B"):
+    """
+    Scale bytes to its proper byte format
+    e.g:
+        1253656 => '1.20MB'
+        1253656678 => '1.17GB'
+    """
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+        if b < factor:
+            return f"{b:.2f}{unit}{suffix}"
+        b /= factor
+    return f"{b:.2f}Y{suffix}"
 
 class GraphicalSorterWindow(QMainWindow):
 
     def __init__(self, left, top, width, height, root):
         super().__init__()
         self.shortcut_folders = dict()
+        self.shortcut_labels = dict()
+        self.shortcut_counters = dict()
         self.root = root
         self.index = 1
-        self.playlist = filter(lambda name: path.isfile(path.join(self.root, name)), iter(os.listdir(self.root)))
+        self.playlist = filter(lambda name: path.isfile(path.join(self.root, name)), iter(listdir(self.root)))
         self.current_video = next(self.playlist)
         self.cur_duration = 0
         self.cur_position = 0
 
         self.setGeometry(QRect(left, top, width, height))
-
         self.video_widget = QVideoWidget()
-        self.setCentralWidget(self.video_widget)
 
-        self.player = QMediaPlayer()
+        self.h_layout = QHBoxLayout()
+        self.h_layout.setContentsMargins(0, 0, 0, 0)
+
+        grid = QVBoxLayout()
+        grid.addWidget(self.video_widget, stretch=1)
+        grid.addLayout(self.h_layout)
+
+        self_widget = QWidget(self)
+        self_widget.setLayout(grid)
+        self.setCentralWidget(self_widget)
+
+        self.player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
         self.player.durationChanged.connect(self.duration_updater())
         self.player.positionChanged.connect(self.position_updater())
         self.player.setVideoOutput(self.video_widget)
@@ -56,7 +81,8 @@ class GraphicalSorterWindow(QMainWindow):
     def title_update(self):
         position_timestamp = str(timedelta(seconds=self.cur_position // 1000))
         duration_timestamp = str(timedelta(seconds=self.cur_duration // 1000))
-        new_window_title = "{0} {1}/{2} {3}/{4}".format(self.current_video, position_timestamp, duration_timestamp, self.index, len(os.listdir(self.root)) + 1)
+        nb_files = len(listdir(self.root))
+        new_window_title = f"{self.current_video} {position_timestamp}/{duration_timestamp} {self.index}/{nb_files}"
         self.setWindowTitle(new_window_title)
 
     def duration_updater(self):
@@ -76,13 +102,15 @@ class GraphicalSorterWindow(QMainWindow):
         if key in self.shortcut_folders:
             source_path = path.join(self.root, self.current_video)
             self.next_media()
-            move_file_to_dir(source_path, self.shortcut_folders[key])
+            moved_size = move_file_to_dir(source_path, self.shortcut_folders[key])
+            self.shortcut_counters[key] += moved_size
+            self.shortcut_labels[key].setText(path.basename(self.shortcut_folders[key]) + ": " + get_size_format(self.shortcut_counters[key]))
             self.index -= 1
         elif key == Qt.Key_Right:
             self.next_media()
         elif key == Qt.Key_O:
             source_path = path.join(self.root, self.current_video)
-            os.system('"{0}"'.format(source_path))
+            system('"{0}"'.format(source_path))
 
     def next_media(self):
         self.player.stop()
@@ -90,7 +118,7 @@ class GraphicalSorterWindow(QMainWindow):
             self.current_video = next(self.playlist)
             self.index += 1
         except StopIteration:
-            self.playlist = filter(lambda name: path.isfile(path.join(self.root, name)), iter(os.listdir(self.root)))
+            self.playlist = filter(lambda name: path.isfile(path.join(self.root, name)), iter(listdir(self.root)))
             self.current_video = next(self.playlist)
             self.index = 1
         content = QMediaContent(QUrl("file:///" + path.join(self.root, self.current_video)))
@@ -99,6 +127,9 @@ class GraphicalSorterWindow(QMainWindow):
 
     def add_shortcut(self, folder_path, qt_key_ref):
         self.shortcut_folders[qt_key_ref] = folder_path
+        self.shortcut_counters[qt_key_ref] = 0
+        self.shortcut_labels[qt_key_ref] = QLabel(path.basename(folder_path) + ": " + str(0))
+        self.h_layout.addWidget(self.shortcut_labels[qt_key_ref])
 
 
 if __name__ == '__main__':
